45b3a0273781c30cf5abda58927476bc7ed5d359
RealArt-quiz <anders.gronning@gmail.com>
2026-07-17 12:17:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -148,8 +148,7 @@ function renderResults(list) {
       <p><strong>Traek:</strong> ${car.tow || '-'} kg</p>
       <p>
         <a target="_blank" 
-           
-           href="https://www.google.com/search?q=${encodeURIComponent(car.brand + ' ' + car.model + ">Producent</a> |
+href="https://www.google.com/search?q=${encodeURIComponent(car.brand + ' ' + car.model + ">Producent</a> |
         <a target="_blank" href="https://www.google.com/search?q=${encodeURIComponent(car.brand + ' ' + car.model + ' FDM test')}">FDM Test</a> |
         <a target="_blank" href="https://www.google.com/search?q=${encodeURIComponent(car.brand + ' ' + car.model + ' Bil Magasinet test')}">Bil Magasinet</a>
       </p>
